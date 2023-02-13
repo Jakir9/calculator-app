ad0b17561b8d8b2ba1c761fe530785a9aad2341c
Jakir <Jakir9@github.com>
2023-02-13 13:11:05 +0000

    updated calc.js

--- calc.js
+++ calc.js
@@ -12,7 +12,67 @@ const previousKeyType = calc.dataset.previousKeyType;
       const action = key.dataset.action //gets the action associated with the key press i.e add, sub, div, multiply
       const keyContent = key.textContent //saves the key content (used for calcs)
       const displayedNum = display.textContent //used for displaying key press on calc
-    }
+
+        if (
+          action === 'add' ||
+          action === 'subtract' ||
+          action === 'multiply' ||
+          action === 'divide'
+         ) {
+          key.classList.add('is-depressed') //tells us when an operator has been pressed down
+          calc.dataset.previousKeyType = 'operator';
+        }
+
+        else if(action == 'decimal') {
+            displayedNum = displayedNum + "."
+        }
+    
+        else { 
+    
+         if(action == 'add'){
+            const firstValue = calc.dataset.firstValue;
+            const secondValue = displayedNum;
+             add(firstValue, displayedNum);
+
+         } 
+
+        else if(action =='subtract'){
+            const firstValue = calc.dataset.firstValue;
+            const secondValue = displayedNum;
+             sub(firstValue, displayedNum);
+            
+
+        }
+        else if(action == 'multiply'){
+            const firstValue = calc.dataset.firstValue;
+            const secondValue = displayedNum;
+            multiply(firstValue, displayedNum);
+        }
+        else if(action == 'divide'){
+            const firstValue = calc.dataset.firstValue;
+            const secondValue = displayedNum;
+            divide(firstValue, displayedNum);
+        }
+        else if(action == 'calculate'){
+            equal();
+
+        }
+
+        else if(action =='decimal'){
+            display.textContent = displayedNum + '.'
+
+        }
+
+        else if(action =='clear'){
+            console.log('clear pressed');
+            total = 0;
+            return updateTotal();
+            
+        }
+        else {
+            console.log('Button not valid');
+        }
+}       
 
     if(!action){ //Filters out the operators, so we only have the numbers. True = not False = Not Action i.e. its a Number
         if (displayedNum === '0' || previousKeyType === 'operator') { 
@@ -22,88 +82,9 @@ const previousKeyType = calc.dataset.previousKeyType;
           }
        }
 
-    if(action == 'decimal') {
-        displayedNum = displayedNum + "."
-    }
-})
+} });
 
-  //CALCULATION LOGIC
-
-    keys.addEventListener('click', e => {
-        if(e.target.matches('button')) {
-            var num1 = e.target; 
-
-
-            // if(!action) { //if number
-            //         num1 = num1  //concatanates the string to display nums w/ more than 1 digit
-            //    }
-        
-            // if(action == 'decimal') {
-            //     num1 = num1 + "."
-            // }
-            if (
-                action === 'add' ||
-                action === 'subtract' ||
-                action === 'multiply' ||
-                action === 'divide'
-              ) {
-                key.classList.add('is-depressed') //tells us when an operator has been pressed down
-                calc.dataset.previousKeyType = 'operator';
-            }
-
-            
-            } else { //if operator
-            
-                if(action == 'add'){
-                    const firstValue = calc.dataset.firstValue;
-                    const secondValue = displayedNum;
-                     add(firstValue, displayedNum);
-
-                } 
-
-                else if(action =='subtract'){
-                    const firstValue = calc.dataset.firstValue;
-                    const secondValue = displayedNum;
-                     sub(firstValue, displayedNum);
-                    
-
-                }
-                else if(action == 'multiply'){
-                    const firstValue = calc.dataset.firstValue;
-                    const secondValue = displayedNum;
-                    multiply(firstValue, displayedNum);
-                }
-                else if(action == 'divide'){
-                    const firstValue = calc.dataset.firstValue;
-                    const secondValue = displayedNum;
-                    divide(firstValue, displayedNum);
-                }
-                else if(action == 'calculate'){
-                    equal();
-
-                }
-
-                else if(action =='decimal'){
-                    display.textContent = displayedNum + '.'
-
-                }
-
-                else if(action =='clear'){
-                    console.log('clear pressed');
-                    total = 0;
-                    return updateTotal();
-                    
-                }
-                else {
-                    console.log('Button not valid');
-}       
-                }
-
-    }
-
-);
-
-
+  
  keys.addEventListener('click',e => {
     if(e.target.matches('button')) {
         const key = e.target
